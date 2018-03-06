4e668e8da0c9658243d2bc74385f49598f97aefc
amskyrobin <amskyrobinson@gmail.com>
2018-03-06 23:11:20 +0000

    CORS light

--- client/src/Components/Container.js
+++ client/src/Components/Container.js
@@ -6,27 +6,33 @@ class Container extends React.Component {
   }
 
   componentDidMount() {
-    // var xhr = new XMLHttpRequest();
-    // xhr.open("GET", "https://tfe-opendata.com/api/v1/timetables/36235979");
-    // xhr.setRequestHeader(
-    //   "Authorization",
-    //   "Token 8F974D788A6FA7F22AEC511E8D38EC6E"
-    // );
-    // xhr.onload = function() {
-    //   if (xhr.status === 200) {
-    //     console.log("good");
-    //   } else {
-    //     console.log("bad");
-    //   }
-    // };
-    // xhr.send();
-    fetch("https://tfe-opendata.com/api/v1/timetables/36235979", {
-      method: "GET", // or 'PUT'
-      headers: new Headers({
-        "Content-Type": "application/json",
-        Authorization: "Token 8F974D788A6FA7F22AEC511E8D38EC6E"
-      })
-    }).then(res => console.log("response", res));
+    var xhr = new XMLHttpRequest();
+    xhr.open(
+      "GET",
+      "https://cors-anywhere.herokuapp.com/https://tfe-opendata.com/api/v1/stops"
+    );
+    xhr.setRequestHeader(
+      "Authorization",
+      "Token 8F974D788A6FA7F22AEC511E8D38EC6E"
+    );
+
+    xhr.onload = function() {
+      if (xhr.status === 200) {
+        console.log("good");
+        console.log(xhr.responseText);
+      } else {
+        console.log("bad");
+      }
+    };
+    xhr.send();
+    // fetch("https://tfe-opendata.com/api/v1/timetables/36235979", {
+    //   method: "GET", // or 'PUT'
+    //   headers: new Headers({
+    //     "Content-Type": "application/json",
+    //     Authorization: "Token 8F974D788A6FA7F22AEC511E8D38EC6E",
+    //     "Access-Control-Allow-Origin": "http://localhost:3000"
+    //   })
+    // }).then(res => console.log("response", res));
   }
 
   render() {
